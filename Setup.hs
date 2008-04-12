8d8d6a681462ca80eca22523db9aea02aec7ef7a
David Roundy <droundy@darcs.net>
2008-04-12 21:34:09 +0000

    add another (redundant) example requireModule.

--- Setup.hs
+++ Setup.hs
@@ -6,4 +6,5 @@ main = do copyright "David Roundy"
           license "BSD3"
           version "0.0"
           requireModule "System.Posix.Files"
+          requireModule "System.Posix.Env ( setEnv )"
           package "franchise" ["Distribution.Franchise"] >>= build
