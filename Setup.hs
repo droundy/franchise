3365fc81cba8e11f59bef7938395a13b9c6758e1
David Roundy <droundy@darcs.net>
2008-04-17 16:54:20 +0000

    simplify Setup.hs for franchise.

--- Setup.hs
+++ Setup.hs
@@ -3,12 +3,14 @@ import Distribution.Franchise
 
 configure = do copyright "David Roundy"
                license "BSD3"
+               findPackagesFor "Distribution.Franchise"
                addEnv "GHC_FLAGS" "-threaded"
                version "0.0"
 
 buildable = do p <- package "franchise" ["Distribution.Franchise"]
                e <- privateExecutable "sample-setup" "Setup.hs" []
-               return (p .& e)
+               --return (p .& e)
+               return p
 
 main = build configure buildable
 
